00b69e18f35f268f36e0abeea8720f9cec297506
brndnbaek <36093531+brndnbaek@users.noreply.github.com>
2020-07-29 18:43:36 -0400

    boilerplate completed
    - meta data edited
    - working on landing layout

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "title": "Web Starter",
-  "description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PurgeCSS and PostCSS",
-  "url": "https://webstarter.chriscollins.me",
-  "author": "Chris Collins",
-  "meta_data": {
-    "twitter": "@scottishstoater",
-    "default_social_image": "/static/default_social_image.jpg"
-  },
-  "env": process.env.ELEVENTY_ENV === 'production'
-}
+	title: "Bikeinfo",
+	description: "Bikeinfo portal for team korvelo. Cycling team in VA MD",
+	url: "https://bikeinfo.xyz",
+	author: "Brandon Baek",
+	meta_data: {
+		twitter: "@baekpro",
+		default_social_image: "/static/default_social_image.jpg",
+	},
+	env: process.env.ELEVENTY_ENV === "production",
+};
