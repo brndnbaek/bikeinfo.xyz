6a50df27895e5bda9c8ac524f8c9e3fadb825f80
brndnbaek <36093531+brndnbaek@users.noreply.github.com>
2020-08-08 20:07:51 -0400

    intro.njk fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,11 +26,8 @@ module.exports = {
 	},
 	variants: {},
 	plugins: [],
-	purge:
-		process.env.NODE_ENV === "production"
-			? {
+	purge: process.env.NODE_ENV === "production"? {
 					enabled: true,
 					content: ["src/**/*.njk", "src/**/*.js"],
-			  }
-			: {},
-};
+			  } : {}
+}
