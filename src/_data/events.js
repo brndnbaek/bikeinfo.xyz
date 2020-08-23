655bf4ab439c75bfe8908bf4d54a07577f5ddcb0
brndnbaek <36093531+brndnbaek@users.noreply.github.com>
2020-08-23 16:36:42 -0400

    google api key masked

--- src/_data/events.js
+++ src/_data/events.js
@@ -3,7 +3,6 @@
 // require('dotenv').config();
 
 // module.exports = async function() {
-//     let url = 'https://www.googleapis.com/calendar/v3/calendars/hs2ph75jmdfaaoaq2c1pg4mtbg@group.calendar.google.com/events?key=AIzaSyBdnlvDuGuoiZ-nTYqaoIw57KCF27V-y9M'
 //     let urlmasked = 'https://www.googleapis.com/calendar/v3/calendars/${process.env.googleCalendar_ID}/events?key=${process.env.googleCalendar_KEY}'
 
 //     return axios.get(url)
